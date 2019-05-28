bab9bc56a3bd250acb7d718c68beba514d3935ad
wjhwjh <1468017149@qq.com>
2019-05-28 17:09:10 +0800

    添加数据

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -10,6 +10,17 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
 
+/*引入自己的数据*/
+const express = require('express')
+const app = express()
+let appData = require('../data.json')
+let seller = appData.seller
+let goods = appData.goods
+let ratings = appData.ratings
+console.log(goods)
+let apiRouter = express.Router()
+app.use('/api',apiRouter)
+
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
 
@@ -42,6 +53,32 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     quiet: true, // necessary for FriendlyErrorsPlugin
     watchOptions: {
       poll: config.dev.poll,
+    },
+    before(app){
+      app.get('/api/appData', function (req, res) {
+        res.json({
+          errno:0,
+          data:appData
+        })
+      }),
+      app.get('/api/seller', function (req, res) {
+        res.json({
+          errno:0,
+          data:seller
+        })
+      }),
+      app.get('/api/goods', function (req, res) {
+        res.json({
+          errno: 0,
+          data:goods
+        })
+      }),
+      app.get('/api/ratings', function (req, res) {
+        res.json({
+          errno: 0,
+          data:ratings
+        })
+      })
     }
   },
   plugins: [
